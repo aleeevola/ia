01ed2b05a6a6b2673ef31b59e7fff208e1c0254d
Jraviolo <josefina.raviolo@gmail.com>
2020-05-04 12:16:16 -0300

    Creación AgenteCustodia.java

--- src/search/AgenteCustodia.java
+++ src/search/AgenteCustodia.java
@@ -1,5 +1,82 @@
 package search;
 
-public class AgenteCustodia {
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+import frsf.cidisi.faia.agent.Action;
+import frsf.cidisi.faia.agent.Perception;
+import frsf.cidisi.faia.agent.search.Problem;
+import frsf.cidisi.faia.agent.search.SearchAction;
+import frsf.cidisi.faia.agent.search.SearchBasedAgent;
+import frsf.cidisi.faia.solver.search.DepthFirstSearch;
+import frsf.cidisi.faia.solver.search.Search;
+
+public class AgenteCustodia extends SearchBasedAgent{
+    public AgenteCustodia() {
+
+        // The Agent Goal
+        ObjetivoAgenteCustodia agGoal = new ObjetivoAgenteCustodia();
+
+        // The Agent State
+        EstadoAgenteCustodia agState = new EstadoAgenteCustodia();
+        this.setAgentState(agState);
+
+        // Create the operators
+        Vector<SearchAction> operators = new Vector<SearchAction>();	
+        operators.addElement(new IrEste());
+        operators.addElement(new IrOeste());	
+        operators.addElement(new IrNorte());
+        operators.addElement(new IrSur());
+        operators.addElement(new Multar());
+
+        // Create the Problem which the agent will resolve
+        Problem problem = new Problem(agGoal, agState, operators);
+        this.setProblem(problem);
+    }
+
+    /**
+     * This method is executed by the simulator to ask the agent for an action.
+     */
+    @Override
+    public Action selectAction() {
+
+        // Create the search strategy
+    	DepthFirstSearch strategy = new DepthFirstSearch();          
+
+        // Create a Search object with the strategy
+        Search searchSolver = new Search(strategy);
+
+        /* Generate an XML file with the search tree. It can also be generated
+         * in other formats like PDF with PDF_TREE */
+        searchSolver.setVisibleTree(Search.EFAIA_TREE);
+
+        // Set the Search searchSolver.
+        this.setSolver(searchSolver);
+
+        // Ask the solver for the best action
+        Action selectedAction = null;
+        try {
+            selectedAction =
+                    this.getSolver().solve(new Object[]{this.getProblem()});
+        } catch (Exception ex) {
+            Logger.getLogger(AgenteCustodia.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        // Return the selected action
+        return selectedAction;
+
+    }
+
+    /**
+     * This method is executed by the simulator to give the agent a perception.
+     * Then it updates its state.
+     * @param p
+     */
+    @Override
+    public void see(Perception p) {
+        this.getAgentState().updateState(p);
+    }
 
 }
