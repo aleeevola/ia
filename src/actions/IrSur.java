706c611854615b6e47ede7de61fc61cdedc94101
aleeevola <aleeevola@gmail.com>
2020-05-05 14:13:37 -0300

    acciones

--- src/actions/IrSur.java
+++ src/actions/IrSur.java
@@ -4,13 +4,23 @@ import frsf.cidisi.faia.agent.search.SearchAction;
 import frsf.cidisi.faia.agent.search.SearchBasedAgentState;
 import frsf.cidisi.faia.state.AgentState;
 import frsf.cidisi.faia.state.EnvironmentState;
+import search.EstadoAgenteCustodia;
+import search.EstadoAmbiente;
+import search.VectorCalles;
 
 public class IrSur extends SearchAction{
 
 	@Override
 	public SearchBasedAgentState execute(SearchBasedAgentState s) {
-		// TODO Auto-generated method stub
-		return null;
+		EstadoAgenteCustodia agState = (EstadoAgenteCustodia) s;
+        VectorCalles esquinaSiguiente = agState.getEsquinas().get(agState.getUbicacionActual()).get(3); //Get(3) porque el orden de las esquinas siguientes es "Este,Oeste,Norte,Sur"
+        //Las direcciones en las que no pueden moverse el agente desde la esquina en la que está tienen asignado un "0" 
+        if(esquinaSiguiente!=null){
+        	agState.setUbicacionActual(esquinaSiguiente);
+        	return agState;
+        }
+
+        return null;
 	}
 
 	@Override
@@ -20,14 +30,24 @@ public class IrSur extends SearchAction{
 
 	@Override
 	public EnvironmentState execute(AgentState ast, EnvironmentState est) {
-		// TODO Auto-generated method stub
+		EstadoAmbiente environmentState = (EstadoAmbiente) est;
+        EstadoAgenteCustodia agState = ((EstadoAgenteCustodia) ast);
+
+
+        VectorCalles esquinaSiguiente = agState.getEsquinas().get(agState.getUbicacionActual()).get(3); //Get(3) porque el orden de las esquinas siguientes es "Este,Oeste,Norte,Sur"
+        //Las direcciones en las que no pueden moverse el agente desde la esquina en la que está tienen asignado un "0" 
+        if(esquinaSiguiente!=null){
+        	environmentState.setposicionAgenteCustodia(esquinaSiguiente);
+
+        	return environmentState;
+        }
 		return null;
 	}
 
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
-		return null;
+		return "IrSur";
 	}
 
 }
